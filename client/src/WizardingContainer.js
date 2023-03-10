748227cc9ad09873f871c749f0714c2a0e9856d8
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-10 13:55:32 +0000

    basic file structure

---
+++ client/src/WizardingContainer.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+const WizardingContainer = ()=>{
+
+return(
+    <>
+    </>
+)
+}
+
+
+
+
+export default WizardingContainer
