dd3a969aae47d83d0592cb4983f3444d5f9afa06
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-11 14:13:00 +0000

    search now working

--- client/src/components/ElixirsPage/ElixirList.js
+++ client/src/components/ElixirsPage/ElixirList.js
@@ -1,30 +1,48 @@
+import { useState } from "react";
 import ElixirItem from "./ElixirItem";
 
 const ElixirsList = ({ elixirs, handleElixirChanges }) => {
-
-
-
-  const ElixirDifficulty = ['Unknown','Advanced','Moderate','Beginner','OneOfAKind','OrdinaryWizardingLevel'];
-//   const difficultySelecter = () => {
-//     elixirs.forEach((elixir) => {
-//       if (!ElixirDifficulty.includes(elixir.difficulty)) {
-//         let { difficulty } = elixir;
-//         ElixirDifficulty.push(difficulty);
-//       }
-//     });
-//   };
-//   difficultySelecter();
+  const ElixirDifficulty = [
+    "Unknown",
+    "Advanced",
+    "Moderate",
+    "Beginner",
+    "OneOfAKind",
+    "OrdinaryWizardingLevel",
+    "",
+  ];
+  //   const difficultySelecter = () => {
+  //     elixirs.forEach((elixir) => {
+  //       if (!ElixirDifficulty.includes(elixir.difficulty)) {
+  //         let { difficulty } = elixir;
+  //         ElixirDifficulty.push(difficulty);
+  //       }
+  //     });
+  //   };
+  //   difficultySelecter();
 
   const ElixirItems = elixirs.map((elixir) => {
     return <ElixirItem elixir={elixir} key={elixir.id} />;
   });
-  const handleChange= event=>{
-    handleElixirChanges(event.target.value)
-  }
+  const handleChange = (event) => {
+    handleElixirChanges(event.target.value);
+  };
+
+  const [query, setQuery] = useState("");
 
   return (
     <div>
       <h1>Elixirs</h1>
+      <div className="search">
+        <input
+          autocapitalize="word"
+
+          type={"text"}
+          placeholder="Search Here"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+
       <select onChange={handleChange}>
         {ElixirDifficulty.map((difficulty) => {
           return (
@@ -34,7 +52,13 @@ const ElixirsList = ({ elixirs, handleElixirChanges }) => {
           );
         })}
       </select>
-      {ElixirItems}
+      {ElixirItems.filter((list) => {
+        if (query === "") {
+          return list;
+        } else if (list.props.elixir.name.includes(query)) {
+          return list;
+        }
+      })}
     </div>
   );
 };
