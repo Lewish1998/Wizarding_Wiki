fe4aac55ad18ccc99d6b53950d0e6ca27087c3f5
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-03-12 23:48:58 +0000

    added a play button(unsure on placement)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import WizardingContainer from "./Containers/WizardingContainer"
+import sound from './theme/theme.mp3'
+import { useEffect, useState } from "react"
 
 const App = () => {
   
+    function play(){
+        new Audio(sound).play()
+    }
 
     return(
         <div>
+            <button onClick={play}>play</button>
             <WizardingContainer/>
         </div>
     )
