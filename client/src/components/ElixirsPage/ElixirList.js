6ccbaaaaa9311cfd7f68537831f6327a43b6ea42
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2023-03-12 13:47:52 +0000

    copied container from main

--- client/src/components/ElixirsPage/ElixirList.js
+++ client/src/components/ElixirsPage/ElixirList.js
@@ -1,24 +1,3 @@
-// import ElixirItem from "./ElixirItem";
-
-// const ElixirsList = ({elixirs})=>{
-
-//     const ElixirItems = elixirs.sort((a,b) => a.name > b.name ? 1: -1).map((elixir)=>{
-//         return <ElixirItem elixir = {elixir} key={elixir.id}/>
-    
-//     })
-
-//     return(
-//         <div>
-//             <h1>Elixirs</h1>
-//             {ElixirItems}
-            
-//         </div>
-//     )
-// }
-
-// export default ElixirsList;
-
-
 import { useState } from "react";
 import ElixirItem from "./ElixirItem";
 
