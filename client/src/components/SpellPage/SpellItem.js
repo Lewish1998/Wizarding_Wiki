29a734489c4a938c42f8d1aeeda66242f6263b03
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-03-13 14:54:22 +0000

    Update SpellItem.js

--- client/src/components/SpellPage/SpellItem.js
+++ client/src/components/SpellPage/SpellItem.js
@@ -1,17 +1,27 @@
+import React, { useState } from 'react'
 import '../Style/Spells.css'
 
 const SpellItem = ({spell}) => {
-  
+
+    const [open, setOpen] = useState(false)
+
+    const toggle= () => {
+        setOpen(!open)
+    }
 
 return(
     <div id="spell-item">
-        <li>{spell.name}</li>
+        <p onClick={toggle}>{spell.name}</p>
+        {open &&
+        <div>
         <li><b>INCANTATION </b>{spell.incantation}</li>
         <li>EFFECT {spell.effect}</li>
         <li>TYPE {spell.type}</li>
         <li>LIGHT {spell.light}</li>
+        </div>
+        }
     </div>
 )
 }
 
-export default SpellItem
+export default SpellItem;
