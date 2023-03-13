045c715d894849f683f8315a8404b2871818b7ed
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-13 14:57:35 +0000

    able to click ingredients

--- client/src/components/IngredientsPage/IngredientsSelected.js
+++ client/src/components/IngredientsPage/IngredientsSelected.js
@@ -2,12 +2,22 @@ const IngredientSelector = ({ selectedIngredient, elixirsWithIngredients }) => {
   console.log(elixirsWithIngredients);
 
   const ElixirsWithIngredientsItems = elixirsWithIngredients.map((elixir) => {
-    return <div>Used in:<h2><li>{elixir.name}</li></h2> Having the following effect: <br/> <h2>{elixir.effect}</h2> <br/>  </div>;
+    return (
+      <div>
+        Used in:
+        <h2>
+          <li>{elixir.name}</li>
+        </h2>{" "}
+        Having the following effect: <br /> <h3><li>{elixir.effect}</li></h3> Other Secondaries: {elixir.ingredients.map((secondaries)=>{
+            return (<li>{secondaries.name} <br/></li>)
+        })} <br />{" "}
+      </div>
+    );
   });
 
   return (
     <div>
-        <h1>{selectedIngredient.split('%20').join(" ")}</h1>
+      <h1>{selectedIngredient.split("%20").join(" ")}</h1>
       <ul> {ElixirsWithIngredientsItems}</ul>
     </div>
   );
