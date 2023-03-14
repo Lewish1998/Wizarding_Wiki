dba02ae12d1ac5b609d069855f06a0ab2e27fd10
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-14 15:02:15 +0000

    fixed spelling error

--- client/src/components/HousesPage/HouseDetails.js
+++ client/src/components/HousesPage/HouseDetails.js
@@ -3,7 +3,7 @@ import '../Style/Spells.css'
 
 const HouseDetails = ({house}) => {
 
-    const houstTraits = house.traits.map((trait, index) => {
+    const houseTraits = house.traits.map((trait, index) => {
         return `${trait.name} `
     })
     
@@ -12,7 +12,7 @@ const HouseDetails = ({house}) => {
         <h1><u>{house.name}</u></h1>
         <div id='spell-item'>
         <p>One of the four founding houses of Hogwarts initially sponsored by {house.founder}.</p>
-        <p>The students of this house continue to embody the traits of {houstTraits} that they were known for.</p>
+        <p>The students of this house continue to embody the traits of {houseTraits} that they were known for.</p>
         <p>Their house colours of {house.houseColours} and the {house.animal} are reflected in their coat of arms.</p>
         <p>The current head of house is {house.heads[0]['firstName']} {house.heads[0]['lastName']} and the common room in the  {house.commonRoom} is watched over by the ghost  {house.ghost}. 
         </p>
