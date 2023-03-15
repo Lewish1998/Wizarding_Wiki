f8c216a780fd3f16d8dc646b2500b7945effa74f
jammyroll <121103824+jammyroll@users.noreply.github.com>
2023-03-15 11:33:02 +0000

    fina; product back off lewis

--- client/src/components/IngredientsPage/IngredientsList.js
+++ client/src/components/IngredientsPage/IngredientsList.js
@@ -1,70 +1,65 @@
 import { useState } from "react";
 import IngredientItem from "./IngredientsItem";
 import IngredientSelector from "./IngredientsSelected";
-import '../Style/Spells.css'
+import "../Style/Spells.css";
 
-
-const IngredientsList = ({ingredients,onIngredientClicked,selectedIngredient,elixirsWithIngredients}) => {
-
+const IngredientsList = ({
+  ingredients,
+  onIngredientClicked,
+  selectedIngredient,
+  elixirsWithIngredients,
+}) => {
   const [query, setQuery] = useState("");
 
+  const IngredientsItems = ingredients
+    .sort((a, b) => (a.name > b.name ? 1 : -1))
+    .map((ingredient) => {
+      return (
+        <IngredientItem
+          ingredient={ingredient}
+          key={ingredient.id}
+          onIngredientClicked={onIngredientClicked}
+        />
+      );
+    });
 
-    const IngredientsItems = ingredients.sort((a,b) => a.name > b.name ? 1: -1).map((ingredient) => {
-      return <IngredientItem ingredient={ingredient} key={ingredient.id} onIngredientClicked={onIngredientClicked}/>
-    })
-
-    return (
-        <div className='search-bar-container'>
-        <h1 className='spell-heading'>Ingredients</h1>
-        <div className="flex-center-top">
-          <input
-            className='search'
-            autoCapitalize="word"
-            type={"text"}
-            placeholder="Search Here"
-            onChange={(event) => setQuery(event.target.value)}
+  return (
+    <div className="search-bar-container">
+      <h1 className="spell-heading">Ingredients</h1>
+      <div className="flex-center-top">
+        <input
+          className="search"
+          autoCapitalize="word"
+          type={"text"}
+          placeholder="Search Here"
+          onChange={(event) => setQuery(event.target.value)}
+        />
+      </div>
+      <div className="big-grid">
+      <div id="spells-container" className="selected-ingredient" >
+        {selectedIngredient ? (
+          <IngredientSelector
+            selectedIngredient={selectedIngredient}
+            elixirsWithIngredients={elixirsWithIngredients}
           />
-        </div>
-
-          <div id='spells-container'>
-            
-            {selectedIngredient ? <IngredientSelector selectedIngredient={selectedIngredient} elixirsWithIngredients={elixirsWithIngredients} />:null}
+        ) : null}
+      </div>
+      <div id="spells-container" className="all-ingredients">
         {IngredientsItems.filter((list) => {
           if (query === "") {
             return list;
-          } else if (list.props.ingredient.name.toLowerCase().includes(query.toLowerCase())) {
+          } else if (
+            list.props.ingredient.name
+              .toLowerCase()
+              .includes(query.toLowerCase())
+          ) {
             return list;
           }
         })}
-        </div>
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // <div id="ingredients">
-        //     <h1>Ingredients</h1>
-
-        //     <ul>{IngredientsItems}</ul>
-        // </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default IngredientsList;
